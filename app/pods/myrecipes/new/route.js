a018877af85406ab0c8955e31f9c0a537cbfcc2d
Fausto Murillo <fausto.murillo@softtek.com>
2016-08-17 16:11:32 -0700

    cookbook-app
    added some templates

--- app/pods/myrecipes/new/route.js
+++ app/pods/myrecipes/new/route.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('recipe');
+  },
+
+  setupController(controller, model) {
+     this._super(controller, model);
+
+   },
+
+   renderTemplate() {
+     this.render('myrecipes/form');
+   },
+
+  actions: {
+
+    saveRecipe(newRecipe) {
+      newRecipe.save().then(() => this.transitionTo('myrecipes'));
+    },
+
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
