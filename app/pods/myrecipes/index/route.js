201e8161a9dcb9741c8fec08a4789572c60efae6
Fausto Murillo <fausto.murillo@softtek.com>
2016-08-22 15:55:49 -0700

    cookbook-app
    templates

--- app/pods/myrecipes/index/route.js
+++ app/pods/myrecipes/index/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.findAll('recipe');
+  },
+
+  actions: {
+
+    deleteRecipe(recipe) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        recipe.destroyRecord();
+      }
+    }
+  }
+
 });
